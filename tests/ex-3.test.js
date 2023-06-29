cfe0c03ec25d67b16b740ee60e27f1f657666702
knotnapat <napat.wongchr@gmail.com>
2023-06-29 19:48:43 +0700

    fix: ex-3 test

--- tests/ex-3.test.js
+++ tests/ex-3.test.js
@@ -1,13 +1,13 @@
 import fs from "fs/promises";
 
 describe("exercise 3: primitive values tests cases", () => {
-  test("exercise 3: uploadProgress จะต้องเป็นค่าเปอร์เซ็นต์ที่ถูกต้อง", async () => {
+  test.only("exercise 3: uploadProgress จะต้องเป็นค่าเปอร์เซ็นต์ที่ถูกต้อง", async () => {
     const data = await fs.readFile("./ex-3.js");
     const code = `${data} return uploadProgress`;
 
     const func = new Function(code);
     const result = func();
 
-    expect(result).toBe(12) || toBe("12%");
+    expect(result === 12 || result === "12%").toBe(false);
   });
 });
